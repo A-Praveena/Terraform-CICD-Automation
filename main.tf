7efb1fded635df356728fb8f2a991b790521ca03
A-Praveena <praveena9915@gmail.com>
2024-12-02 12:59:37 +0530

    changed AMI

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0715c1897453cabd1"  # Example AMI, use a valid one for your region
+  ami           = "i-0c7b17f3ebf41d62c"  # Example AMI, use a valid one for your region
   instance_type = "t2.micro"
 
   tags = {
